be266a14b2cdc4d74c1b760fc5c526f6c82ab044
Zchwazer <yamiyomuyumi@gmail.com>
2020-03-03 14:17:51 +0700

    Create radio button & checkbox
    make radio button & checkbox into method 
    (Use quick fix to extract method after you finish coding)

--- dart/lab2.dart
+++ dart/lab2.dart
@@ -1,50 +1,50 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+// final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: MyWidget(),
-        ),
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: MyWidget(),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-       mainAxisSize: MainAxisSize.max,
-      children: [
-        BlueBox(),
-        BlueBox(),
-        BlueBox(),
-      ],
-    );
-  }
-}
+// class MyWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//        mainAxisSize: MainAxisSize.max,
+//       children: [
+//         BlueBox(),
+//         BlueBox(),
+//         BlueBox(),
+//       ],
+//     );
+//   }
+// }
 
-class BlueBox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        border: Border.all(),
-      ),
-    );
-  }
-}
+// class BlueBox extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 50,
+//       height: 50,
+//       decoration: BoxDecoration(
+//         color: Colors.blue,
+//         border: Border.all(),
+//       ),
+//     );
+//   }
+// }
